563937a29cba6e2916ed0abd91f9af2de2a6651e
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2024-06-03 21:26:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ def calculate_profit_loss(jumlah_lot, harga_beli, harga_jual, fee_beli, fee_jual
     profit_loss_percentage = (profit_loss / total_beli) * 100 if total_beli else 0
     return total_beli, total_jual, profit_loss, profit_loss_percentage
 
+def calculate_dividend(jumlah_lot, dividen_per_saham):
+    total_dividen = jumlah_lot * dividen_per_saham * 100
+    return total_dividen
+
+def calculate_dividend_yield(dividen_per_saham, harga_beli):
+    dividend_yield = (dividen_per_saham / harga_beli) * 100
+    return dividend_yield
+
 def stock_page(title, fee_beli, fee_jual):
     st.header(title)
     jumlah_lot = st.number_input("Jumlah Lot:", min_value=0, step=1, format="%d")
@@ -14,6 +22,17 @@ def stock_page(title, fee_beli, fee_jual):
     harga_jual = st.number_input("Harga Jual (per saham):", min_value=0.0, step=1000.0, format="%.0f")
     include_fee_beli = st.checkbox("Masukkan Fee Beli")
     include_fee_jual = st.checkbox("Masukkan Fee Jual")
+    include_dividend = st.checkbox("Masukkan Dividen")
+
+    if include_dividend:
+        dividen_per_saham_input = st.number_input("Dividen per Saham:", min_value=0, step=1, format="%d")
+        if jumlah_lot > 0 and harga_beli > 0:
+            total_dividen = calculate_dividend(jumlah_lot, dividen_per_saham_input)
+            dividend_yield = calculate_dividend_yield(dividen_per_saham_input, harga_beli)
+            st.write(f"Total Dividen: Rp {total_dividen:,.0f}")
+            st.write(f"Dividend Yield: {dividend_yield:.2f}%")
+        else:
+            st.warning("Harap masukkan jumlah lot dan harga beli yang benar untuk menghitung dividen.")
 
     if st.button("Hitung"):
         if jumlah_lot >= 0 and harga_beli >= 0 and harga_jual >= 0:
@@ -39,8 +58,8 @@ menu_selection = st.sidebar.radio("Pilih Menu:", ("Welcome", "IPOT / Indo Premie
 # Routing based on menu selection
 if menu_selection == "Welcome":
     st.title("Welcome!")
-    st.write(" Selamat datang di aplikasi kalkulator profit/loss saham. ")
-    st.write(" 👈 Silahkan pilih aplikasi yang digunakan pada menu sidebar. 👈")
+    st.write("Selamat datang di aplikasi kalkulator profit/loss saham.")
+    st.write("👈 Silahkan pilih aplikasi yang digunakan pada menu sidebar. 👈")
 elif menu_selection == "IPOT / Indo Premier Sekuritas":
     stock_page("IPOT / Indo Premier Sekuritas", 0.0019, 0.0029)
 elif menu_selection == "Stockbit / Bibit":
