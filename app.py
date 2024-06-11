af600ec02efdd411f412ac9046449c171b4e3c2f
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2024-06-12 02:24:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,9 @@ if menu_selection == "Welcome":
     st.write("Selamat datang di aplikasi kalkulator profit/loss saham.")
     st.write("👈 Silahkan pilih aplikasi yang digunakan pada menu sidebar. 👈")
     st.markdown('<div style="float:right;">'
-                '<a href="https://www.facebook.com/yuukinaesa/" target="_blank"><button style="background-color:#3b5998;color:white;border-radius:5px;padding:10px 15px;border:none;margin-right:5px;">Facebook</button></a>'
-                '<a href="https://www.instagram.com/yuukinaesa/" target="_blank"><button style="background-color:#e4405f;color:white;border-radius:5px;padding:10px 15px;border:none;margin-right:5px;">Instagram</button></a>'
-                '<a href="https://id.linkedin.com/in/yuukinaesa/" target="_blank"><button style="background-color:#0e76a8;color:white;border-radius:5px;padding:10px 15px;border:none;">LinkedIn</button></a>'
+                '<a href="https://www.facebook.com/yuukinaesa/"><button style="background-color:#3b5998;color:white;border-radius:5px;padding:10px 15px;border:none;margin-right:5px;">Facebook</button></a>'
+                '<a href="https://www.instagram.com/yuukinaesa/"><button style="background-color:#e4405f;color:white;border-radius:5px;padding:10px 15px;border:none;margin-right:5px;">Instagram</button></a>'
+                '<a href="https://id.linkedin.com/in/yuukinaesa/"><button style="background-color:#0e76a8;color:white;border-radius:5px;padding:10px 15px;border:none;">LinkedIn</button></a>'
                 '</div>', unsafe_allow_html=True)
 elif menu_selection == "IPOT / Indo Premier Sekuritas":
     stock_page("IPOT / Indo Premier Sekuritas", 0.0019, 0.0029)
