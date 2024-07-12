3b7996a6d8150a6931096bc545e08d38b4965716
Arfan Hidayat Priyantono <arfan.hidayat.priyantono@gmail.com>
2024-07-12 21:38:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def calculate_dividend_yield(dividen_per_saham, harga_beli):
     dividend_yield = (dividen_per_saham / harga_beli) * 100
     return dividend_yield
 
-def stock_page(title, fee_beli, fee_jual):
+def stock_page(title, default_fee_beli, default_fee_jual):
     st.header(title)
     jumlah_lot = st.number_input("Jumlah Lot:", min_value=0, step=1, format="%d")
     harga_beli = st.number_input("Harga Beli (per saham):", min_value=0.0, step=1000.0, format="%.0f")
@@ -23,6 +23,9 @@ def stock_page(title, fee_beli, fee_jual):
     include_fee_beli = st.checkbox("Masukkan Fee Beli")
     include_fee_jual = st.checkbox("Masukkan Fee Jual")
     include_dividend = st.checkbox("Masukkan Dividen")
+
+    fee_beli = default_fee_beli if include_fee_beli else 0
+    fee_jual = default_fee_jual if include_fee_jual else 0
 
     if include_dividend:
         dividen_per_saham_input = st.number_input("Dividen per Saham:", min_value=0, step=1, format="%d")
@@ -35,9 +38,7 @@ def stock_page(title, fee_beli, fee_jual):
             st.warning("Harap masukkan jumlah lot dan harga beli yang benar untuk menghitung dividen.")
 
     if st.button("Hitung"):
-        if jumlah_lot >= 0 and harga_beli >= 0 and harga_jual >= 0:
-            fee_beli = fee_beli if include_fee_beli else 0
-            fee_jual = fee_jual if include_fee_jual else 0
+        if jumlah_lot > 0 and harga_beli > 0 and harga_jual >= 0:
             total_beli, total_jual, profit_loss, profit_loss_percentage = calculate_profit_loss(jumlah_lot, harga_beli, harga_jual, fee_beli, fee_jual)
             st.write(f"Total Beli: Rp {total_beli:,.0f}")
             st.write(f"Total Jual: Rp {total_jual:,.0f}")
@@ -64,7 +65,6 @@ if menu_selection == "Welcome":
     **@Yuukinaesa** \n
                     Arfan Hidayat Priyantono ✅
     ''')
-
 
 elif menu_selection == "IPOT / Indo Premier Sekuritas":
     stock_page("IPOT / Indo Premier Sekuritas", 0.0019, 0.0029)
